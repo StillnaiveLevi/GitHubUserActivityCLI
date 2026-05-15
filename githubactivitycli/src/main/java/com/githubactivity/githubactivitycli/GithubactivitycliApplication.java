8d286f047b39bee0c219cd1846ac2812b0ec17d5
StillnaiveLevi <merkebbirhane3@gmail.com>
2026-05-15 15:07:56 +0300

    setting up project

---
+++ githubactivitycli/src/main/java/com/githubactivity/githubactivitycli/GithubactivitycliApplication.java
@@ -0,0 +1,13 @@
+package com.githubactivity.githubactivitycli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactivitycliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactivitycliApplication.class, args);
+	}
+
+}
---
+++ githubactivitycli/src/test/java/com/githubactivity/githubactivitycli/GithubactivitycliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubactivity.githubactivitycli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactivitycliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
